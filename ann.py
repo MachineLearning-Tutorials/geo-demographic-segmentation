56669c5babfbb4150e30ae588b9445bdf43ad89d
Vivian Rajkumar <paintfrussianblue@gmail.com>
2017-05-20 09:07:02 +0800

    Added ANN

---
+++ ann.py
@@ -0,0 +1,74 @@
+# artifical neural network
+
+# data preprocessing
+
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# encode categorical independent variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+# dummy variables
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# split dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# ANN
+
+# import Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# initialize ANN
+classifier = Sequential()
+
+# add input layer and first hidden layer
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform', input_shape = (11,)))
+
+# add second hidden layer
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer = 'uniform'))
+
+# add output layer
+classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'uniform'))
+
+# compile ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# fit ANN to training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# make predictions and evaluate model
+
+# predict test set results
+y_pred = classifier.predict(X_test)
+
+# convert probabilities to true/false
+y_pred = (y_pred > 0.5)
+
+# create confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
